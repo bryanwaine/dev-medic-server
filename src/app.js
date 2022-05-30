f0ab37ea002ff7dfb052c8bd449c28e284602b1d
bryan waine <nwanne.ezeaka@gmail.com>
2022-05-30 14:34:16 +0100

    added start script

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const sendContactEmail = require('./utils/sendContactEmail');
+const dotenv = require('dotenv').config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
