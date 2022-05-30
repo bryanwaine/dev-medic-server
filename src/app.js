0b7df61e0e345436b197866d95fa109b95c0299f
bryan waine <nwanne.ezeaka@gmail.com>
2022-05-30 14:02:21 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const sendContactEmail = require('./utils/sendContactEmail');
 const app = express();
 const port = 4000;
 
-// middleware
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
