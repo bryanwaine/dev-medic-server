7f3df240357616195b8092a79ae61b2c364daf8c
bryan waine <nwanne.ezeaka@gmail.com>
2022-05-30 13:55:10 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const sendContactEmail = require('./utils/sendContactEmail');
+
+const app = express();
+const port = 4000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.post('/', (req, res) => {
+  try {
+    sendContactEmail(req.body.name, req.body.email, req.body.message);
+    return res.status(200).send({
+      message: `Message submitted successfully!`,
+    });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
